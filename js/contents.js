d48a895f08b731608ec763e680e71735b55cb4f5
ShortArrow <bamboogeneral@live.jp>
2020-12-01 14:23:53 +0900

    logo design origin

--- js/contents.js
+++ js/contents.js
@@ -1,13 +1,17 @@
+chrome.storage.sync.set({ color: item }, function () {
+    console.log('color is ' + item);
+});
+
 let url = location.href;
 let fake = url.split("/");
 let targetIndex = fake.indexOf("dp") + 1;
-if (targetIndex==0) {
+if (targetIndex == 0) {
     alert('このページのURLは短縮出来ないようです。');
 }
-else{
-    let ShortURL="https://www.amazon.co.jp/dp/" + fake[targetIndex];
-    if(navigator.clipboard){
+else {
+    let ShortURL = "https://www.amazon.co.jp/dp/" + fake[targetIndex];
+    if (navigator.clipboard) {
         navigator.clipboard.writeText(ShortURL);
     }
-    alert("「"+ShortURL+"」をクリップボードにコピーしました。");
+    alert("「" + ShortURL + "」をクリップボードにコピーしました。");
 }
