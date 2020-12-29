4d00d4d3e4366edfcb01b2f9bb032a635dd79383
ShortArrow <bamboogeneral@live.jp>
2020-12-29 17:17:24 +0900

    change url macher

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: chrome.i18n.getMessage("app_config__localize_url") },
+                pageUrl: { hostPrefix: chrome.i18n.getMessage("app_config__localize_url") },
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
